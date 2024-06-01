bda6b6d9fdc3d110bfec1fd29c970fe99daaa6f3
Andre M. de Souza <andresouza44@gmail.com>
2024-06-01 16:08:18 -0300

    melhorias na autenticação

--- src/main/java/com/andre/forum_hub/config/SecurityConfiguration.java
+++ src/main/java/com/andre/forum_hub/config/SecurityConfiguration.java
@@ -6,9 +6,9 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,7 +24,7 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
-
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
     @Value("${cors.origins}")
@@ -36,20 +36,29 @@ public class SecurityConfiguration {
     @Autowired
     SecurityFilter securityFilter;
 
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST,"/auth/register").permitAll()
-                        .requestMatchers(HttpMethod.POST,"/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/topicos").hasAnyRole("ADMIN")
-                        .anyRequest().authenticated())
+                        .anyRequest().permitAll())
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
-
     }
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        return httpSecurity.csrf(csrf -> csrf.disable())
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers(HttpMethod.POST,"/auth/register").permitAll()
+//                        .requestMatchers(HttpMethod.POST,"/auth/login").permitAll()
+//                        .requestMatchers(HttpMethod.POST, "/topicos").hasAnyRole("ADMIN")
+//                        .anyRequest().authenticated())
+//                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+//                .build();
+//
+//    }
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
--- src/main/java/com/andre/forum_hub/controller/TopicoController.java
+++ src/main/java/com/andre/forum_hub/controller/TopicoController.java
@@ -8,12 +8,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-
 import java.net.URI;
-import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping(value = "/topicos")
@@ -22,6 +21,7 @@ public class TopicoController {
     @Autowired
     private TopicoService service;
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<TopicoDto> create(@RequestBody @Valid TopicoDto dto) {
         TopicoDto dtoResult = service.cadastrar(dto);
@@ -33,6 +33,7 @@ public class TopicoController {
 
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
     @GetMapping
     public ResponseEntity<Page<TopicoDto>> findAll(@PageableDefault(size=10,sort = "dataCriacao") Pageable pageable,
                                                    @RequestParam(name ="curso", defaultValue = "") String curso,
@@ -41,12 +42,14 @@ public class TopicoController {
         return ResponseEntity.ok().body(topicos);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
     @GetMapping(value = "/{id}")
     public ResponseEntity<TopicoDto> findById(@PathVariable Long id){
         TopicoDto dto = service.findById(id);
         return ResponseEntity.ok().body(dto);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PutMapping(value = "/{id}")
     public ResponseEntity<TopicoDto> update (@RequestBody @Valid TopicoDto dto, @PathVariable  Long id){
         dto = service.update(dto, id);
@@ -54,6 +57,7 @@ public class TopicoController {
 
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity delete (@PathVariable Long id){
         service.deleteById(id);
